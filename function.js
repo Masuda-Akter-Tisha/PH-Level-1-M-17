efef6d220df605cf048d58d79d56259d4a612f92
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-02-16 21:00:15 +0600

    added function.js file

---
+++ function.js
@@ -0,0 +1,9 @@
+//function declare
+function makeupKoro (){
+    console.log('blusher use koro');
+    console.log('glitter use koro');
+    console.log('do not use eyelash');
+}
+
+//call the function
+makeupKoro();
